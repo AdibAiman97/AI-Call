c72076b74a4af606fd1fd519fcccb592c8cad414
AdibAiman <adaiman3269@gmail.com>
2025-05-19 14:45:40 +0800

    UI Database for Voxis-AI features

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,5 +19,6 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/Database': RouteRecordInfo<'/Database', '/Database', Record<never, never>, Record<never, never>>,
   }
 }
