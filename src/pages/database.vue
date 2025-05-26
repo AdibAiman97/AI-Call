fd72f99bc08f677e67c6b6403c791674ac21ac2b
AdibAiman <adaiman3269@gmail.com>
2025-05-26 13:23:16 +0800

    Changes for format

--- src/pages/database.vue
+++ src/pages/database.vue
@@ -4,14 +4,14 @@
       <!-- Header Section -->
       <v-row class="pa-6" justify="space-between" align="center">
         <h1 class="text-h3 font-weight-bold">Import Note to Database</h1>
-        <v-btn variant="outlined" size="small" color="grey-lighten-1">
+        <v-btn variant="outlined" color="grey-lighten-1" class="mr-2" style="min-width:120px;">
           <v-icon size="x-large" class="mr-1">mdi-eye-circle</v-icon>
           <p class="text-capitalize ma-0">View FAQ Database</p>
         </v-btn>
       </v-row>
       <!-- Subtext -->
       <v-row class="px-6">
-        <p class="text-grey-darken-1 mb-0">Add new information to the database</p>
+        <p>Add new information to the database</p>
       </v-row>
       <br>
       <br>
@@ -20,11 +20,11 @@
   <v-col cols="12" md="12" lg="12" xl="11">
     <v-card class="pa-6" elevation="2" width="100%" variant="outlined" color="#1F2937" border-color="grey-lighten-2">
       <!-- Context Section -->
-      <p class="text-h6 pa-2 text-foreground">Context:</p>
+      <p class="text-h6  text-foreground">Context:</p>
+      <br>
       <v-textarea
         rows="10"
         auto-grow
-        class="pa-2"
         label="Input Text.."
         style=" color: #fff;"
         input-class="text-white"
@@ -32,7 +32,7 @@
       <!-- Buttons -->
       <v-row class="mt-2" justify="start" align="center" no-gutters>
         <v-col cols="auto">
-          <v-btn variant="outlined" color="grey" class="mr-2" style="min-width:120px;">
+          <v-btn variant="outlined" color="grey-lighten-1" class="mr-2" style="min-width:120px;">
             <span class="text-capitalize">Cancel</span>
           </v-btn>
         </v-col>
