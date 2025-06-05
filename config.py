f2ed6601890ce63df1bb38de290a241a198aed9c
hafizrein <hafizrein@gmail.com>
2025-06-05 13:12:36 +0800

    modified config file so that the project id is Voxis-AI

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ GEMINI_API_KEY = ""  # API KEY
 GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
 
 
-GCP_PROJECT_ID = "my-project-adib-395502"
+GCP_PROJECT_ID = "voxis-ai"
 GCP_LOCATION = "us-central1"
