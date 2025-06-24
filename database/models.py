e44dbe9d408e524b8e114c298f430e56918a12af
hafizrein <hafizrein@gmail.com>
2025-06-24 13:31:33 +0800

    cleaned up the requirements.txt file and moved the db configuration to dotenv file

--- database/models.py
+++ database/models.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
 
-# Database configuration
-DB_USERNAME = "postgres"
-DB_PASSWORD = "password"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "customer_db"
+load_dotenv()
+
+# Get database configuration from environment variables
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
 DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Database setup
